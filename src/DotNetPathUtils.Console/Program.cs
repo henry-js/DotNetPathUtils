e4c7289a44cb824b4c4a729f122bd98fef1e11ab
henry-js <79054685+henry-js@users.noreply.github.com>
2025-07-14 02:59:53 +0100

    add console sample

---
+++ src/DotNetPathUtils.Console/Program.cs
@@ -0,0 +1,10 @@
+﻿using DotNetPathUtils;
+using Velopack;
+
+VelopackApp.Build().Run();
+
+IEnvironmentService service = new SystemEnvironmentService();
+Console.WriteLine("Created SystemEnvironmentService instance.");
+
+var envHelper = new PathEnvironmentHelper(service);
+Console.WriteLine("Created PathEnvironmentHelper instance.");
